44f28e489ace27d0eab1797a994220740b2f2f1b
Har1Prasaath <shwethahari005@gmail.com>
2025-03-18 19:42:53 +0530

    Security Changes Made

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,20 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD95B0KOSiHUpptyJLajQfuG7knNGIx5HU",
-  authDomain: "plms-e2f70.firebaseapp.com",
-  databaseURL: "https://plms-e2f70-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "plms-e2f70",
-  storageBucket: "plms-e2f70.firebasestorage.app",
-  messagingSenderId: "193745647848",
-  appId: "1:193745647848:web:3f259e589a6f3a6e2df15d",
-  measurementId: "G-EPZ1ND16KK"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
